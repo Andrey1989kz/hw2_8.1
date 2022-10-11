9a41a883f5391a62fdc457168b376ef662e9e1f9
Fisatidi Andrey <fissatidi@gmail.ru>
2022-10-11 21:08:40 +0500

    commit

---
+++ src/main/java/com/example/hw2_8_1/Hw281Application.java
@@ -0,0 +1,13 @@
+package com.example.hw2_8_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw281Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw281Application.class, args);
+    }
+
+}
